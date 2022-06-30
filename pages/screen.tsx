7fcd13990e3fe290ec60ea0fa12803abd5c8ec3d
Vadim <vmakeev.dev@gmail.com>
2022-06-30 15:57:11 +0400

    Add validation

--- pages/screen.tsx
+++ pages/screen.tsx
@@ -4,10 +4,40 @@ import styles from '../styles/Home.module.css'
 import InputMask from 'react-input-mask';
 
 const Form = styled.div`
-border: 1px solid black;
+border: 3px solid #711C91;
 display: flex;
 flex-direction: column;
 padding: 10px;
+border-radius: 10px
+`
+
+const Title = styled.div`
+font-family: 'Finlandica', sans-serif;
+font-size: 24px;
+text-align: center;
+font-weight: 700;
+background-color: #711C91;
+color: white;
+padding: 3px 0;
+border-radius: 50px;
+`
+
+const Button = styled.input`
+background-color: #711C91;
+font-family: 'Finlandica', sans-serif;
+border: none;
+padding: 3px 0;
+color: white;
+border-radius: 50px;
+font-weight: 700;
+font-size: 24px;
+text-transform: uppercase;
+&:hover{
+background-color: #800080;
+}
+&:active{
+background-color: #BA55D3;
+}
 `
 
 const Label = styled.label`
@@ -15,10 +45,20 @@ font-size: 20px;
 font-family: 'Finlandica', sans-serif;
 `
 
+const Alert = styled.div`
+margin-top: -5px;
+margin-bottom: 5px;
+color: #711C91;
+font-weight: 600;
+font-family: 'Finlandica', sans-serif;
+`
+
 const Screen = (props: any) => {
 
-    const [phone, setPhone] = useState('')
+    const [phone, setPhone] = useState('');
     const [money, setMoney] = useState('');
+    const [isVisible, setIsVisible] = useState(false)
+    const [isVisibleMoney, setIsVisibleMoney] = useState(false)
 
     function onChange(e: { target: {name: string, value: string} }){
         const target = e.target;
@@ -30,11 +70,22 @@ const Screen = (props: any) => {
         }
     }
 
+    function Pay(){
+        const isValid = phone.length === 18;
+        setIsVisible(!isValid)
+        const numb = Number(money.substring(2))
+        const isValidMoney = numb >= 1 && numb <= 1000;
+        setIsVisibleMoney(!isValidMoney)
+        if(isValid && isValidMoney){
+            
+        }
+    }
+
     return(
         <div className={styles.container}>
             <div className={styles.main}>
                 <Form>
-                    МТС
+                    <Title>МТС</Title>
                     <Label htmlFor='phone'>
                         Введите номер телефона:
                     </Label>
@@ -44,9 +95,13 @@ const Screen = (props: any) => {
                         onChange={onChange}
                         name='phone'
                         value={phone}
+                        maskChar=''
                         mask='+7 (999) 999 99 99'
                         placeholder='8 (999) 000-00-00'
                     />
+                    {isVisible &&
+                        <Alert>Введите верный телефон</Alert>
+                    }
                     <Label htmlFor='price'>
                         Укажите сумму:
                     </Label>
@@ -58,7 +113,15 @@ const Screen = (props: any) => {
                         name='money'
                         value={money}
                         mask={'₽ 9999'}
-                        placeholder='0 ₽'
+                        placeholder='₽'
+                    />
+                    {isVisibleMoney &&
+                    <Alert>Укажите сумму от 1 до 1000</Alert>
+                    }
+                    <Button
+                        onClick={Pay}
+                        value='Оплатить'
+                        type='button'
                     />
                 </Form>
             </div>
